f9e5a267e0221d421073f067fd3fcd3fd3d003b5
Michelle Lu <mlu7@terpmail.umd.edu>
2023-11-04 17:33:56 -0400

    add setters

--- app/src/main/java/com/example/project6/Pong.kt
+++ app/src/main/java/com/example/project6/Pong.kt
@@ -1,5 +1,28 @@
 package com.example.project6
 
-class Pong {
+import android.graphics.Point
 
+class Pong {
+    private var ballRadius = 0
+    private var ballCenter : Point? = null
+    private var ballAngle = 45
+    private var ballSpeed = 0f
+    constructor(newBallRadius : Int, newBallSpeed : Float) {
+        setBallSpeed(newBallSpeed)
+        setBallRadius(newBallRadius)
+
+
+    }
+
+    fun setBallSpeed(newBallSpeed : Float) {
+        if (newBallSpeed > 0) {
+            ballSpeed = newBallSpeed
+        }
+    }
+
+    fun setBallRadius(newBallRadius : Int) {
+        if (newBallRadius > 0) {
+            ballRadius = newBallRadius
+        }
+    }
 }
