36f3d9b39bfdd1111b3fff7335af3688a914f6af
Michelle Lu <mlu7@terpmail.umd.edu>
2023-11-04 16:53:08 -0400

    start mainActivity

--- app/src/main/java/com/example/project6/MainActivity.kt
+++ app/src/main/java/com/example/project6/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.project6
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
+import java.util.Timer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // need to comment out?
+        // setContentView(R.layout.activity_main)
+
+        var width : Int = Resources.getSystem().displayMetrics.widthPixels
+        var height : Int = Resources.getSystem().displayMetrics.heightPixels
+
+        // retrieve status bar height
+        var statusBarId : Int = resources.getIdentifier( "status_bar_height", "dimen", "android" )
+        var statusBarHeight : Int  = resources.getDimensionPixelSize( statusBarId )
+
+        gameView = GameView( this, width, height - statusBarHeight )
+        game = gameView.getGame()
+        setContentView( gameView )
+        /*
+        // set up event handling
+        var handler : TouchHandler = TouchHandler( )
+        detector = GestureDetector( this, handler )
+        detector.setOnDoubleTapListener( handler )
+        */
+
+        /*
+        // set schedule
+        var gameTimer : Timer = Timer( )
+        var gameTimerTask : GameTimerTask = GameTimerTask( this )
+        gameTimer.schedule( gameTimerTask, 0L, GameView.DELTA_TIME.toLong() )
+        */
     }
+
 }
