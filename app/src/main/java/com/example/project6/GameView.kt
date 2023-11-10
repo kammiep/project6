cd35af5a4683fb07b7d6fbd6d065167bbdd3c2ec
Michelle Lu <mlu7@terpmail.umd.edu>
2023-11-09 21:30:00 -0500

    print score

--- app/src/main/java/com/example/project6/GameView.kt
+++ app/src/main/java/com/example/project6/GameView.kt
@@ -1,12 +1,15 @@
 package com.example.project6
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Rect
 import android.util.Log
 import android.view.View
+import android.widget.RelativeLayout
+import android.widget.TextView
 
 class GameView : View {
     private lateinit var paint : Paint
@@ -14,6 +17,7 @@ class GameView : View {
     private var height : Int  = 0
     private lateinit var paddle: Rect
     private lateinit var pong : Pong
+    private lateinit var context : Context
     constructor(context: Context, width:Int, height:Int) : super(context){
         this.width = width
         this.height = height
@@ -25,6 +29,7 @@ class GameView : View {
 
         paddle = Rect(width/2-100,height-100,width/2+100,height-80)
         pong = Pong(width,height, width/2-100, height-100, width/2+100, height-80)
+        this.context = context
     }
 
     fun getPong():Pong{
@@ -42,6 +47,21 @@ class GameView : View {
                 pong.getBallCenter().x.toFloat(), pong.getBallCenter().y.toFloat(),
                 pong.getBallRadius(), paint
             )
+        } else {
+            val score = pong.getScore()
+            var displayText : String = "Final Score: " + score
+            // get data from persistent data
+            var pref : SharedPreferences = context.getSharedPreferences(context.packageName + "_preferences", Context.MODE_PRIVATE)
+            val best = pref.getInt("best", 0)
+            if (score < best) {
+                displayText += "\n\nNOT your best score"
+            } else {
+                displayText += "\n\nNew best score!"
+                var editor = pref.edit()
+                editor.putInt("best", score)
+            }
+            paint.setTextSize(60F);
+            canvas.drawText(displayText, 18F, 60F, paint)
         }
     }
 }
